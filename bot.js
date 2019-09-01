fc98375ef2e03fadce416c16e3c7fd7e2fa523fb
BeraBera156 <54773857+BeraBera156@users.noreply.github.com>
2019-09-01 16:25:16 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("584219070665195539")
 setInterval(function() {
-channel.send(`Bera Bera Rabe Rabe Rabe Bera **[ " ${x} " ]** `);
+channel.send(`**Bera Bera Rabe Rabe Rabe Bera [ " ${x} " ]** `);
 }, 30)
 })
 
